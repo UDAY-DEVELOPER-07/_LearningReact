1c551b9213dc80351f957a440c3e283b38ad2390
UDAY-DEVELOPER-07 <udaysinghno2005@gmail.com>
2024-12-24 18:01:02 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [changeColor, setColor] = useState("Black")
 
+  
   return (
     <>
-      <h1 className='bg-red-600 text-blue-300'>Uday Pratap Singh</h1>
+    <div className='w-full h-screen duration-200 flex items-end  justify-center  p-10' style={{backgroundColor: changeColor}}>
+{/* // onclick want that we pass a funtion to and if we directly pass setColor("red"). it will be a reference to it 
+// thats why we use a callback for it */}
+      <div className='bg-white flex align-baseline bottom-10 rounded-md border-2 border-black' > 
+        <button onClick={() => setColor("Red")} className='h-4.5 w-15 m-2  bg-red-600'  >Red</button>
+        <button onClick={() => setColor("Yellow")} className='h-4.5 w-full p-1 m-2 bg-yellow-400'>Yellow</button>
+        <button onClick={() => setColor("Green")} className='h-4.5 w-15  p-1 m-2 bg-green-600'>Green</button>
+        <button onClick={() => setColor("Blue")} className='h-4.5 w-15  p-1 m-2 bg-blue-600'> Blue</button>
+        <button onClick={() => setColor("Olive")} className='h-4.5 w-15 p-1 m-2 bg-yellow-600' >Olive</button>
+        <button onClick={() => setColor("Gray")} className='h-4.5 w-15 p-1 m-2 bg-gray-600'>Gray</button>
+        <button onClick={() => setColor("Pink")} className='h-4.5 w-15 p-1 m-2 bg-pink-600'>Pink</button>
+        <button onClick={() => setColor("Purple")} className='h-4.5 w-15 p-1 m-2 bg-purple-600'>Purple</button>
+        <button onClick={() => setColor("Lavender")} className='h-4.5 w-15 p-1 m-2 bg-[rgb(220,208,255)]'>Lavender</button>
+        <button onClick={() => setColor("White")} className='h-4.5 w-15 p-1 m-2 bg-white text-black'>White</button>
+        <button onClick={() => setColor("Black")} className='h-4.5 w-15 p-1 m-2 bg-black text-white'>Black</button>
+      </div>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Comp from './components/comp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Comp username="Uday Singh" btn="This" />
   </StrictMode>,
 )
